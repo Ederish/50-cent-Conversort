4d03c058bfdac3dcb76bba07ff981840c165fa99
Ederish <63983547+Ederish@users.noreply.github.com>
2024-11-04 14:19:50 -0600

    Update scrpt.js

--- scrpt.js
+++ scrpt.js
@@ -1,6 +1,6 @@
 var flags = ["img/AR.jpg", "img/CL.png","img/CO.png","img/CU.png","img/DO.png","img/MX.png","img/UY.png"]
 var latamFlag = document.getElementById("latam-flag");
-var apiKey ="";
+var apiKey ="7b783d9b754f45088795c0ef730f6e4f";
 
 async function fetchCurrency() {
       const api = `https://api.currencyfreaks.com/v2.0/rates/latest?apikey=${apiKey}&format=json&symbols=ARS,CLP,COP,CUC,DOP,MXN,UYU`;
