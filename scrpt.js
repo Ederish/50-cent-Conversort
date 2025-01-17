7c154b349586b8708915e879cb0bba2c90dd1dc9
Ederish <63983547+Ederish@users.noreply.github.com>
2025-01-16 19:38:29 -0600

    Update scrpt.js

--- scrpt.js
+++ scrpt.js
@@ -1,9 +1,9 @@
-var flags = ["img/AR.jpg", "img/CL.png","img/CO.png","img/CU.png","img/DO.png","img/MX.png","img/UY.png"]
+var flags = ["img/AR.jpg", "img/CL.png","img/CO.png","img/CU.png","img/DO.png","img/MX.png","img/UY.png","img/VE.png"]
 var latamFlag = document.getElementById("latam-flag");
 var apiKey ="7b783d9b754f45088795c0ef730f6e4f";
 
 async function fetchCurrency() {
-      const api = `https://api.currencyfreaks.com/v2.0/rates/latest?apikey=${apiKey}&format=json&symbols=ARS,CLP,COP,CUC,DOP,MXN,UYU`;
+      const api = `https://api.currencyfreaks.com/v2.0/rates/latest?apikey=${apiKey}&format=json&symbols=ARS,CLP,COP,CUC,DOP,MXN,UYU,VES`;
       const response = await fetch(api);
       const data = await response.json();
       return data.rates;
@@ -43,6 +43,10 @@ async function fetchCurrency() {
         case "UY":
           coins.innerHTML = Math.floor(coinsApi["UYU"] / 2) + " Pesos";
           latamFlag.src = flags[6];
+          break;
+          case "VE":
+          coins.innerHTML = Math.floor(coinsApi["VES"] / 2) + " Bolivares";
+          latamFlag.src = flags[7];
           break;
         default:
           coins.innerHTML = Math.floor(coinsApi["ARS"] / 2);
